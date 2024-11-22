6c0801f072ba1be876d49db3de440b979aed975f
306a <dpmeichen@gmail.com>
2024-11-22 20:12:50 +0800

    deploy

---
+++ _app/immutable/entry/start.BbEOn3B_.js
@@ -0,0 +1,1 @@
+import{f as a}from"../chunks/entry.B1oa5Qla.js";export{a as start};
---
+++ _app/immutable/nodes/2.DnbKxwIU.js
@@ -0,0 +1,1 @@
+import{a as t,t as o}from"../chunks/disclose-version.B5gKXr-b.js";import"../chunks/legacy.CAAz0Voe.js";var p=o('<section class="p-2"><p>I am a software engineer.</p> <p>Contact me at: dpmeichen@gmail.com</p></section>');function c(a){var e=p();t(a,e)}export{c as component};
